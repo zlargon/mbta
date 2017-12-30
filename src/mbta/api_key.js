bce896111a2355246d617ecaae03dede6e615e67
zlargon <zlargon1988@gmail.com>
2017-12-29 20:29:31 -0500

    implement MBTA prediction API

---
+++ src/mbta/api_key.js
@@ -0,0 +1,1 @@
+module.exports = "af310295e7234d658d69cc63ebdef6b4";
---
+++ src/mbta/prediction.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+const api_key = require('./api_key');
+
+module.exports = function (stop_id, direction_id) {
+
+  const url = `https://api-v3.mbta.com/predictions?`
+            + `filter[stop]=${stop_id}&`
+            + `filter[direction_id]=${direction_id}&`
+            + `sort=arrival_time&`
+            + `api_key=${api_key}`;
+
+  return fetch(url)
+          .then(res => res.json())
+          .then(json => json.data.map(o => new Date(o.attributes.departure_time)))
+}
+
