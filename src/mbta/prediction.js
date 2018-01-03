762f956049a4ddd66a07e88941c378707f8f300b
zlargon <zlargon1988@gmail.com>
2018-01-03 15:16:54 -0500

    [mbta] add filer[route] and limit[page]
    https://groups.google.com/forum/#!topic/massdotdevelopers/hmWE_FJCX0Y

--- src/mbta/prediction.js
+++ src/mbta/prediction.js
@@ -6,7 +6,9 @@ module.exports = function (stop_id, direction_id) {
   const url = `https://api-v3.mbta.com/predictions?`
             + `filter[stop]=${stop_id}&`
             + `filter[direction_id]=${direction_id}&`
+            + `filter[route]=Orange&`
             + `sort=departure_time&`
+            + `page[limit]=5&`
             + `api_key=${api_key}`;
 
   return fetch(url)
