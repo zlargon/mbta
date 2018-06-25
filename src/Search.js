9c4fa2171b8febd1a04f4c4d102be60293771489
zlargon <zlargon1988@gmail.com>
2018-06-25 06:54:59 -0400

    implement Search page

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,110 @@
 import React from 'react';
+import Routes from './mbta/route.json';
+
+// List
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+
+// Icons
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import StarIcon from '@material-ui/icons/Star';
+import logo from './logo.png';
 
 class Search extends React.Component {
+
+  state = {
+    open: [],
+    select: {}
+  }
+
+  collapseHandler = (index) => (event, target) => {
+    const open = Object.assign([], this.state.open);
+    open[index] = !open[index];
+
+    this.setState({
+      open: open
+    });
+  }
+
+  selectHandler = (stop_id, direction_id) => () => {
+
+    const select = Object.assign([], this.state.select);
+    select[stop_id + '_' + direction_id] = !select[stop_id + '_' + direction_id];
+
+    this.setState({
+      select: select
+    })
+  }
+
   render () {
+
+    const list = [];
+    for (let route of Routes) {
+
+      // Route list
+      list.push(
+        <ListItem button key={route.id + '-route'}
+          style={{ backgroundColor: '#' + route.color, opacity: 0.9 }}
+          onClick={this.collapseHandler(route.id)}>
+
+          <ListItemIcon>
+            <img alt='' src={logo} style={{ height: '30px', width: '30px' }}/>
+          </ListItemIcon>
+          <ListItemText inset
+            primary={
+              <Typography variant="subheading" style={{ color: '#' + route.text_color }}>
+                {route.name}
+              </Typography>
+            }
+            secondary={
+              <Typography style={{ color: '#' + route.text_color }}>
+                {`${route.direction[0]}/${route.direction[1]}`}
+              </Typography>
+            }/>
+          {this.state.open[route.id] ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+        </ListItem>
+      );
+
+      let stops = [];
+      for (let stop of route.stops) {
+        stops.push(
+          <ListItem key={stop.id}>
+
+            <ListItemText inset primary={stop.name} />
+
+
+            <IconButton onClick={this.selectHandler(stop.id, 0)}>
+              { this.state.select[stop.id + '_0'] ? <StarIcon /> : <StarBorderIcon /> }
+            </IconButton>
+            <IconButton onClick={this.selectHandler(stop.id, 1)}>
+              { this.state.select[stop.id + '_1'] ? <StarIcon /> : <StarBorderIcon /> }
+            </IconButton>
+
+          </ListItem>
+        );
+      }
+
+      // Collapse with stop list
+      list.push(
+        <Collapse key={route.id + '-collapse'} in={this.state.open[route.id]} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            { stops }
+          </List>
+        </Collapse>
+      );
+    }
+
     return (
-      <div style={{ border: '1px solid blue' }}>
-      </div>
+      <List component="nav">
+        { list }
+      </List>
     );
   }
 }
