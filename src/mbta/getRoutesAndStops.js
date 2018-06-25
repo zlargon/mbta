aff8c40677699866c2a446a114de3229783a9a2b
zlargon <zlargon1988@gmail.com>
2018-06-25 06:54:31 -0400

    [data] add getRoutesAndStops.js and route.json

---
+++ src/mbta/getRoutesAndStops.js
@@ -0,0 +1,45 @@
+const _async = require('co').wrap;
+const fetch = require('node-fetch');
+const host = 'https://api-v3.mbta.com';
+
+const getRoutesAndStops = _async(function * () {
+  let res = yield fetch(host + '/routes').then(res => res.json());
+  const routes = res.data.slice(0, 7);
+
+  for (let route of routes) {
+
+    route.name = route.attributes.long_name;
+    route.color = route.attributes.color;
+    route.text_color = route.attributes.text_color;
+    route.direction = route.attributes.direction_names;
+
+    delete route.links;
+    delete route.type;
+    delete route.attributes;
+
+    let stops = yield fetch(host + '/stops?filter[route]=' + route.id)
+                      .then(res => res.json())
+                      .then(res => res.data);
+
+    for (let stop of stops) {
+
+      stop.name = stop.attributes.name;
+      stop.location = {
+        address: stop.attributes.address,
+        latitude: stop.attributes.latitude,
+        longitude: stop.attributes.longitude
+      };
+
+      delete stop.links;
+      delete stop.relationships;
+      delete stop.type;
+      delete stop.attributes;
+    }
+
+    route.stops = stops;
+  }
+
+  return routes;
+})
+
+module.exports = getRoutesAndStops;
