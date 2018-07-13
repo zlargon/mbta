50b1f2e0b3be9f2ace852a04a87c77a9d1567ecd
zlargon <zlargon1988@gmail.com>
2018-07-13 09:50:14 +0800

    [Favorite] if Favorite List is empty, show the instruction of how to add schedules

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -8,11 +8,16 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import Divider from '@material-ui/core/Divider';
 
+// Icon Button
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 // Utils
 import ScheduleListItems from './ScheduleListItems';
+
+// Instructions
+import instruction_1 from '../images/instruction_1.png';
+import instruction_2 from '../images/instruction_2.png';
 
 class Favorite extends React.Component {
   state = {}
@@ -28,68 +33,90 @@ class Favorite extends React.Component {
     });
   }
 
+  getListItems = () => {
+    return Object.values(this.props.schedules).map((sch, index) => {
+      const id = 'sch-' + index;
+      const { route, stop, direct_id, destination, isFailed } = sch;
+      const shortNameStyle = {
+        height: '35px',
+        width: '35px',
+        lineHeight: '36px',
+        textAlign: 'center',
+        borderRadius: '50%',
+        backgroundColor: 'white',
+        opacity: 0.7,
+        marginRight: '5px',
+        fontWeight: 'bold',
+        fontAize: '20px',
+        color: route.color
+      };
+
+      const header = (
+        <ListItem key={id} style={{ backgroundColor: route.color, color: route.text_color, opacity: 0.9 }} >
+
+          { route.short_name === '' ? '' : (
+            <div style={shortNameStyle}>
+              {route.short_name}
+            </div>
+          )}
+
+          <ListItemText inset
+            primary={`${stop.name} → ${destination.name} ${isFailed ? '(Update Failed)' : ''}`}
+            secondary={route.direction[direct_id]}
+
+            primaryTypographyProps={{
+              style: {
+                color: route.text_color
+              }
+            }}
+            secondaryTypographyProps={{
+              style: {
+                color: route.text_color,
+                fontWeight: 'bold'
+              }
+            }}
+          />
+
+          <ListItemSecondaryAction>
+            <IconButton>
+              <DeleteIcon onClick={this.openDialog(sch)} style={{ color: route.text_color }}/>
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      );
+
+      return [
+        header,
+        ScheduleListItems(sch, this.props.currentTime, this.props.maxNumber),
+        <Divider key={id + '-divider'}/>
+      ];
+    });
+  }
+
   render = () => {
+    const imgWidth = {
+      maxWidth: '280px',
+      width: '48%'
+    };
+
+    const listItems = this.getListItems();
     return (
       <List style={this.props.style}>
-        {
-          Object.values(this.props.schedules).map((sch, index) => {
-            const id = 'sch-' + index;
-            const { route, stop, direct_id, destination, isFailed } = sch;
-            const shortNameStyle = {
-              height: '35px',
-              width: '35px',
-              lineHeight: '36px',
-              textAlign: 'center',
-              borderRadius: '50%',
-              backgroundColor: 'white',
-              opacity: 0.7,
-              marginRight: '5px',
-              fontWeight: 'bold',
-              fontAize: '20px',
-              color: route.color
-            };
-
-            const header = (
-              <ListItem key={id} style={{ backgroundColor: route.color, color: route.text_color, opacity: 0.9 }} >
-
-                { route.short_name === '' ? '' : (
-                  <div style={shortNameStyle}>
-                    {route.short_name}
-                  </div>
-                )}
-
-                <ListItemText inset
-                  primary={`${stop.name} → ${destination.name} ${isFailed ? '(Update Failed)' : ''}`}
-                  secondary={route.direction[direct_id]}
-
-                  primaryTypographyProps={{
-                    style: {
-                      color: route.text_color
-                    }
-                  }}
-                  secondaryTypographyProps={{
-                    style: {
-                      color: route.text_color,
-                      fontWeight: 'bold'
-                    }
-                  }}
-                />
-
-                <ListItemSecondaryAction>
-                  <IconButton>
-                    <DeleteIcon onClick={this.openDialog(sch)} style={{ color: route.text_color }}/>
-                  </IconButton>
-                </ListItemSecondaryAction>
-              </ListItem>
-            );
-
-            return [
-              header,
-              ScheduleListItems(sch, this.props.currentTime, this.props.maxNumber),
-              <Divider key={id + '-divider'}/>
-            ];
-          })
-        }
+        { listItems.length > 0 ? listItems : (
+          <ListItem>
+            <ListItemText
+              disableTypography
+              primary={<div>
+                <h2>Your Favorite List is empty.</h2>
+                <p>Please click "Search" button below and add your schedules.</p>
+                <div>
+                  <img alt="" src={instruction_1} style={{...imgWidth, marginRight: '10px'}} />
+                  <img alt="" src={instruction_2} style={imgWidth} />
+                </div>
+              </div>}
+            />
+          </ListItem>
+        )}
       </List>
     );
   }
